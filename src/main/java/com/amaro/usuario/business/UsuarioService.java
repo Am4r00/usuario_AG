561ae1aa06439655829505126a34263880f27497
joao amaro <joaovictoramaroalves@gmail.com>
2026-02-05 18:17:38 -0300

    Criando a função de editar endereços e telefones

--- src/main/java/com/amaro/usuario/business/UsuarioService.java
+++ src/main/java/com/amaro/usuario/business/UsuarioService.java
@@ -1,28 +1,31 @@
 package com.amaro.usuario.business;
 
+import com.amaro.usuario.business.dto.EnderecoDTO;
+import com.amaro.usuario.business.dto.TelefoneDTO;
 import com.amaro.usuario.business.dto.UsuarioDTO;
+import com.amaro.usuario.infrastructure.entity.Endereco;
+import com.amaro.usuario.infrastructure.entity.Telefone;
 import com.amaro.usuario.infrastructure.entity.Usuario;
 import com.amaro.usuario.infrastructure.exceptions.ConflictException;
 import com.amaro.usuario.infrastructure.exceptions.ResourceNotFoundException;
+import com.amaro.usuario.infrastructure.repository.EnderecoRepository;
+import com.amaro.usuario.infrastructure.repository.TelefoneRepository;
 import com.amaro.usuario.infrastructure.repository.UsuarioRepository;
 import com.amaro.usuario.infrastructure.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
-
-    public UsuarioService(UsuarioRepository usuarioRepository, UsuarioConverter usuarioConverter, PasswordEncoder passwordEncoder,JwtUtil jwtUtil) {
-        this.usuarioRepository = usuarioRepository;
-        this.usuarioConverter = usuarioConverter;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtUtil = jwtUtil;
-    }
+    private final EnderecoRepository enderecoRepository;
+    private final TelefoneRepository telefoneRepository;
 
     public UsuarioDTO cadastrarUsuario(UsuarioDTO usuarioDTO){
         checarEmail(usuarioDTO.getEmail());
@@ -53,6 +56,22 @@ public class UsuarioService {
 
         Usuario usuario = usuarioConverter.checkUser(dto, usuarioEntity);
         return usuarioConverter.paraUsuarioDTO(usuario);
+    }
+
+    public EnderecoDTO atualizarEndereco(Long id, EnderecoDTO dto){
+        Endereco enderecoEntity = enderecoRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Endereço não encontrado !"));
+        Endereco endereco = usuarioConverter.checkEndereco(dto, enderecoEntity);
+
+        return usuarioConverter.paraEnderecoDTO(enderecoRepository.save(endereco));
+    }
+
+    public TelefoneDTO atualizarTelefone(Long id, TelefoneDTO dto){
+        Telefone telefoneEntity = telefoneRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Telefone não encontrado !"));
+        Telefone telefone = usuarioConverter.checkTelefone(dto, telefoneEntity);
+
+        return usuarioConverter.paraTelefoneDTO(telefoneRepository.save(telefone));
     }
 
     private void checarEmail(String email) {
--- src/main/java/com/amaro/usuario/controller/UsuarioController.java
+++ src/main/java/com/amaro/usuario/controller/UsuarioController.java
@@ -1,8 +1,12 @@
 package com.amaro.usuario.controller;
 
 import com.amaro.usuario.business.UsuarioService;
+import com.amaro.usuario.business.dto.EnderecoDTO;
 import com.amaro.usuario.business.dto.LoginDTO;
+import com.amaro.usuario.business.dto.TelefoneDTO;
 import com.amaro.usuario.business.dto.UsuarioDTO;
+import com.amaro.usuario.infrastructure.entity.Telefone;
+import com.amaro.usuario.infrastructure.repository.EnderecoRepository;
 import com.amaro.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -51,4 +55,13 @@ public class UsuarioController {
         return ResponseEntity.ok(usuarioService.atualizarUser(token,usuarioDTO));
     }
 
+    @PutMapping("/endereco")
+    public ResponseEntity<EnderecoDTO> atualizarEndereco (@RequestBody EnderecoDTO enderecoDTO, @RequestParam("id") Long id){
+        return ResponseEntity.ok(usuarioService.atualizarEndereco(id,enderecoDTO));
+    }
+
+    @PutMapping("/telefone")
+    public ResponseEntity<TelefoneDTO> atualizarTelefone(@RequestBody TelefoneDTO telefoneDTO, @RequestParam("id") Long id){
+        return ResponseEntity.ok(usuarioService.atualizarTelefone(id,telefoneDTO));
+    }
 }
