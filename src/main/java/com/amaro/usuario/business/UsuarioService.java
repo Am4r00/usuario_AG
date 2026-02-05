7444005c4fb2800955f41057902533a538e0819b
joao amaro <joaovictoramaroalves@gmail.com>
2026-02-05 15:29:59 -0300

    Implemetando o método de buscar,deletar e fazer login

--- src/main/java/com/amaro/usuario/business/UsuarioService.java
+++ src/main/java/com/amaro/usuario/business/UsuarioService.java
@@ -2,22 +2,57 @@ package com.amaro.usuario.business;
 
 import com.amaro.usuario.business.dto.UsuarioDTO;
 import com.amaro.usuario.infrastructure.entity.Usuario;
+import com.amaro.usuario.infrastructure.exceptions.ConflictException;
+import com.amaro.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.amaro.usuario.infrastructure.repository.UsuarioRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
-    private  final UsuarioConverter usuarioConverter;
+    private final UsuarioConverter usuarioConverter;
+    private final PasswordEncoder passwordEncoder;
 
+    public UsuarioService(UsuarioRepository usuarioRepository, UsuarioConverter usuarioConverter, PasswordEncoder passwordEncoder) {
+        this.usuarioRepository = usuarioRepository;
+        this.usuarioConverter = usuarioConverter;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public UsuarioDTO cadastrarUsuario(UsuarioDTO usuarioDTO){
+        checarEmail(usuarioDTO.getEmail());
+
         Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
+        usuario.setSenha(passwordEncoder.encode(usuario.getPassword()));
         usuarioRepository.save(usuario);
 
         return usuarioConverter.paraUsuarioDTO(usuario);
+    }
+
+    public UsuarioDTO buscarUsuarioPorEmail(String email){
+        Usuario usuario =  usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("Email Não encontrado !" + email));
+        return usuarioConverter.paraUsuarioDTO(usuario);
+    }
+
+    public void deletarPorEmail(String email){
+        usuarioRepository.deleteByEmail(email);
+    }
+
+
+    private void checarEmail(String email) {
+        try {
+            boolean existe = verificarEmailExiste(email);
+            if (existe)
+                throw new ConflictException("Email já cadastrado");
+        } catch (Exception e) {
+            throw new ConflictException("Email já cadastrado", e.getCause());
+        }
+    }
+
+    private boolean verificarEmailExiste(String email) {
+        return usuarioRepository.existsByEmail(email);
     }
 }
--- src/main/java/com/amaro/usuario/controller/UsuarioController.java
+++ src/main/java/com/amaro/usuario/controller/UsuarioController.java
@@ -2,17 +2,26 @@ package com.amaro.usuario.controller;
 
 import com.amaro.usuario.business.UsuarioService;
 import com.amaro.usuario.business.dto.UsuarioDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
+@RequiredArgsConstructor
 public class UsuarioController {
 
-    private UsuarioService usuarioService;
+    private final UsuarioService usuarioService;
 
     @PostMapping
     public ResponseEntity<UsuarioDTO> cadastrarUsuario(@RequestBody UsuarioDTO usuarioDTO){
         return ResponseEntity.ok(usuarioService.cadastrarUsuario(usuarioDTO));
     }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<UsuarioDTO> buscarUsuario(@PathVariable String email){
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
+    }
+
+
 }
